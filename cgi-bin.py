5ca79ea09c4ce5f560af3d6b89857feafc33ffdb
Trigrab <trigrab@gmail.com>
2019-02-19 15:30:28 +0100

    improve: script does somethin for real now lmao

--- cgi-bin.py
+++ cgi-bin.py
@@ -1,10 +1,15 @@
 #!/usr/local/bin/python3
 import cgi
+from epson.epson import Epson
 
 print("Content-Type: text/html")    # HTML is following
 print()
 print("<TITLE>CGI script output</TITLE>")
 print("<H1>This is my first CGI script</H1>")
 arguments = cgi.FieldStorage()
+if 'Funktion' in arguments:
+        epson = Epson()
+        epson.send_command(arguments['Funktion'])
+
 for i in arguments.keys():
         print(arguments[i].value)
